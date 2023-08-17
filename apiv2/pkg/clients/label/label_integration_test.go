82609f6d5ce3278f9876aa0921f45803f14f69a9
zhanghaizhou <1078760300@qq.com>
2023-08-17 09:51:15 +0800

    update

--- apiv2/pkg/clients/label/label_integration_test.go
+++ apiv2/pkg/clients/label/label_integration_test.go
@@ -6,8 +6,8 @@ import (
 	"context"
 	"testing"
 
-	"github.com/mittwald/goharbor-client/v5/apiv2/model"
-	clienttesting "github.com/mittwald/goharbor-client/v5/apiv2/pkg/testing"
+	"github.com/TonyZZhang/goharbor-client/v5/apiv2/model"
+	clienttesting "github.com/TonyZZhang/goharbor-client/v5/apiv2/pkg/testing"
 	"github.com/stretchr/testify/require"
 )
 
